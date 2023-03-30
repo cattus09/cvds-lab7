45ab4e93ff688fcbe3ac31154b7fb9e64b1783ec
sergio-g-v <sergio.gonzalez-v@mail.escuelaing.edu.co>
2023-03-30 14:10:25 -0500

    Ya

---
+++ cvds-lab7/src/test/java/com/sergio/gonzalez/cvdslab7/CvdsLab7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergio.gonzalez.cvdslab7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvdsLab7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
